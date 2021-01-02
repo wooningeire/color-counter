e6ab7dae4186c62d6c50de4a40206958c577921d
​ <>
2021-01-02 09:56:44 -0500

    Change whitespace

--- worker.js
+++ worker.js
@@ -1,18 +1,18 @@
 addEventListener("message", event => {
-    const imageDataArray = event.data;
+	const imageDataArray = event.data;
 
-    const set = new Set();
+	const set = new Set();
 
-    for (let i = 0; i < imageDataArray.length; i += 4) {
-        // Convert color to primitive (ARGB)
-        const color =
-                (BigInt(imageDataArray[i + 3]) << 8n * 3n) +
-                (BigInt(imageDataArray[i]) << 8n * 2n) +
-                (BigInt(imageDataArray[i + 1]) << 8n) +
-                (BigInt(imageDataArray[i + 2]));
+	for (let i = 0; i < imageDataArray.length; i += 4) {
+		// Convert color to primitive (ARGB)
+		const color =
+				(BigInt(imageDataArray[i + 3]) << 8n * 3n) +
+				(BigInt(imageDataArray[i]) << 8n * 2n) +
+				(BigInt(imageDataArray[i + 1]) << 8n) +
+				(BigInt(imageDataArray[i + 2]));
 
-        set.add(color);
-    }
+		set.add(color);
+	}
 
-    postMessage(set.size);
+	postMessage(set.size);
 });
